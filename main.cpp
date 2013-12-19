aea3eb97c199c329ca58269c65746a895b74c6fb
azja <andrzej.biborski@gmail.com>
2013-12-19 15:22:25 +0100

    styling

--- main.cpp
+++ main.cpp
@@ -21,22 +21,22 @@
 
 int main() {
 
-double q = 2.0;
-double g0 = 2.0;
+    double q = 2.0;
+    double g0 = 2.0;
 
- for(int i = 1, j = 0; i <= N; i = 2*(++j) + 1) { /* i in {2k+1} */
+    for(int i = 1, j = 0; i <= N; i = 2*(++j) + 1) { /* i in {2k+1} */
 
-    std::cout<<"#Gaussian: "<<i<<" Result:       ";
-    GMinimizer minimizer(i);                  /*minimizer*/
-    const gsl_vector  *result =  &minimizer.solve(q, g0, 1.0e-6);
+        std::cout<<"#Gaussian: "<<i<<" Result:       ";
+        GMinimizer minimizer(i);                  /*minimizer*/
+        const gsl_vector  *result =  &minimizer.solve(q, g0, 1.0e-6);
 
-    q = gsl_vector_get(result,0);
-    g0 =gsl_vector_get(result, 1);
+        q = gsl_vector_get(result,0);
+        g0 =gsl_vector_get(result, 1);
 
-    std::cout<<"q = "<<q<< " g_0 = "<<g0<<" E_0 = "<<gsl_vector_get(result, 2)<<std::endl;
- }
+        std::cout<<"q = "<<q<< " g_0 = "<<g0<<" E_0 = "<<gsl_vector_get(result, 2)<<std::endl;
+    }
 
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 
